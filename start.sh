144562bea5d769a5d5038773a53e211283a6cbe5
pmo <philmoses@gmail.com>
2020-04-13 12:26:36 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ __create_user() {
 # Create a user to SSH into as.
 useradd sshuser 
 SSH_USERPASS=newpass
-echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin sshuser)
+echo -e "$SSH_USERPASS" | (passwd --stdin sshuser)
 echo ssh user password: $SSH_USERPASS
 }
 
